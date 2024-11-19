da7c435344b56af0a5ddc8d56e4bcae4396687af
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2024-11-18 21:22:39 -0500

    test(validators): add unit tests for custom Angular validators
    This commit introduces comprehensive testing for `MyValidators` to ensure custom validation logic works as expected.
    
    Validator: `validPassword`
    - **Test Cases:**
      - Returns `null` when the password meets the validation criteria.
      - Returns an object `{ invalid_password: true }` when the password is invalid.
    
    Validator: `matchPasswords`
    - **Test Cases:**
      - Returns `null` when the `password` and `confirmPassword` fields match.
      - Returns an object `{ match_password: true }` when the fields do not match.
      - Throws an error when either `password` or `confirmPassword` is undefined, with a clear error message: "matchPasswords: fields not found."
    
    These updates provide robust coverage for the custom validators, ensuring they function correctly and handle edge cases gracefully.

---
+++ src/app/utils/validators.spec.ts
@@ -0,0 +1,64 @@
+import { FormControl, FormGroup } from "@angular/forms";
+import { MyValidators } from "./validators";
+
+fdescribe('test for my validators', () => {
+
+  describe('test for valid password', () => {
+
+    it('should return null when password is right', () => {
+
+      const control = new FormControl();
+      control.setValue('nicolas123');
+
+      const rta = MyValidators.validPassword(control);
+
+      expect(rta).toBeNull();
+    })
+
+    it('should return an object with key invalid_password:true when password is wrong', () => {
+
+      const control = new FormControl();
+      control.setValue('nicolas');
+
+      const rta = MyValidators.validPassword(control);
+
+      expect(rta?.invalid_password).toBeTrue();
+    })
+
+  })
+
+  describe('test for match password', () => {
+
+    it('should return null when both are equal', () => {
+      const group = new FormGroup({
+        password: new FormControl('test'),
+        confirmPassword: new FormControl('test')
+      })
+
+      const rta = MyValidators.matchPasswords(group);
+      expect(rta).toBeNull();
+    })
+
+    it('should return an object with key match_password: true when both not match', () => {
+      const group = new FormGroup({
+        password: new FormControl('test'),
+        confirmPassword: new FormControl('test2')
+      })
+
+      const rta = MyValidators.matchPasswords(group);
+      expect(rta?.match_password).toBeTrue();
+    })
+
+    it('should return an error when some password or confirmPassword is not defined', () => {
+      const group = new FormGroup({
+        otherfield: new FormControl('test'),
+        confirmPassword: new FormControl('test2')
+      })
+
+      const fn= ()=>{MyValidators.matchPasswords(group)}
+      expect(fn).toThrow(new Error('matchPasswords: fields not found'));
+    })
+
+  })
+
+})
